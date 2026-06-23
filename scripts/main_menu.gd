094c02d5716fd9632e4fba0bdb909183c361cf56
¨Odaniel22¨ <odanitocbba@gmail.com>
2026-06-22 21:31:33 -0400

    Implementacion del menu para elegir personaje

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,8 +2,8 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/arena.tscn")
-
+	#get_tree().change_scene_to_file("res://scenes/arena.tscn")
+	get_tree().change_scene_to_file("res://scenes/EleccionPersonaje.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
